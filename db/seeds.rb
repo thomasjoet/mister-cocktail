0ae6a3d6b9796108ad3711bb7cf3b9d4651f2b2a
Thomas Joët <thomas.joet.fr@gmail.com>
2015-10-30 12:37:01 +0100

    deleted cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,13 @@
+Cocktail.destroy_all
+
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "orange")
 Ingredient.create(name: "chocolate")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "whisky")
+Ingredient.create(name: "gin")
 
 
 # This file should contain all the record creation needed to seed the database with its default values.
